f76d54f8aeddef7d766454c6852a62dbf6127eab
LeeJunyoung <hellotranshuman@gmail.com>
2018-03-26 22:05:19 +0900

    source code : initial state

---
+++ Capstone_Design_Team5/resources/assets/js/components/user/user_main/UserMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div> 
+        <h1>성준아 메인페이지 구현해줘!!!</h1>
+        <p>여기 주소 resources/assets/js/components/user/user_main/UserMain.vue</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
